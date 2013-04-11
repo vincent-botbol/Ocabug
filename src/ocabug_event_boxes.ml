ada1386179f651bdd9219b9caa6182fc4a0c4cfb
CHAILLOUX <root@debian2.(none)>
2013-04-11 19:06:55 +0200

    Event boxes now stored in a hash table instead of a list.

--- src/ocabug_event_boxes.ml
+++ src/ocabug_event_boxes.ml
@@ -1,54 +1,95 @@
 open Ocabug_view
 open Source_viewer
+open Instruct
+open Hashtbl
+
+exception No_such_event_box
+
+type event_box =
+    { mutable click : bool; (* to know if we want to create or
+			       remove a breakpoint *)
+      mutable break_number : int;
+      mutable counter : int; (* how many times we go through this event *)
+      image : GMisc.image;
+      event : Instruct.debug_event
+    }
+
+let event_boxes : (int, event_box) Hashtbl.t = Hashtbl.create 30
 
 let ebox_list :
     (int * (bool ref * int ref * GMisc.image * Instruct.debug_event)) list ref
     = ref []
 
 let add_ebox n img ev =
-  ebox_list := (n, (ref true, ref (-1), img, ev)) :: !ebox_list
+  add event_boxes n
+    { click = true;
+      break_number = -1;
+      counter = 0;
+      image = img;
+      event = ev
+    }
 
-let ebox_from_event event =
-  List.find (fun (_,(_,_,_,ev)) -> ev = event) !ebox_list
+let ebox_from_event ev =
+  let res = ref None in
+  try
+    Hashtbl.iter (fun _ ebox ->
+      if ebox.event.ev_pos = ev.ev_pos then
+	begin res := Some ebox; raise Exit end)
+      event_boxes;
+    raise No_such_event_box
+  with
+    | Exit -> (match !res with Some x -> x | _ -> raise No_such_event_box)
 
 let ebox_from_break_number break_number =
-  List.find (fun (_,(_,bn,_,_)) -> break_number = !bn) !ebox_list
+  let res = ref None in
+  try
+    Hashtbl.iter (fun _ ebox ->
+      if ebox.break_number = break_number then
+	begin res := Some ebox; raise Exit end)
+      event_boxes;
+    raise No_such_event_box
+  with
+    | Exit -> (match !res with Some x -> x | _ -> raise No_such_event_box)
 
-let set_break click img bn new_bn =
-  assert !click;
-  img#set_pixbuf breakpoint_pixbuf;
-  bn := new_bn;
-  click := false
+let set_break ebox new_bn =
+  assert ebox.click;
+  ebox.image#set_pixbuf breakpoint_pixbuf;
+  ebox.break_number <- new_bn;
+  ebox.click <- false
 
 let set_break_from_event event break_number =
-  let (_,(click,bn,img,_)) = ebox_from_event event in
-  set_break click img bn break_number
+  set_break (ebox_from_event event) break_number
 
 let set_break_from_ebox_number ebox_number break_number =
-  let (click,bn,img,_) = List.assoc ebox_number !ebox_list in
-  set_break click img bn break_number
+  try
+    set_break (Hashtbl.find event_boxes ebox_number) break_number
+  with
+      Not_found -> raise No_such_event_box
 
     (* bn is the reference containing the associated breakpoint number with the event *)
-let remove_break click bn img =
-  assert (!click = false);
-  img#set_pixbuf event_pixbuf;
-  bn := -1;
-  click := true
+let remove_break ebox =
+  assert (not ebox.click);
+  ebox.image#set_pixbuf event_pixbuf;
+  ebox.break_number <- -1;
+  ebox.click <- true
 
 let remove_break_from_number break_number =
-  let (_,(click,bn,img,_)) = ebox_from_break_number break_number in
-  remove_break click bn img
+  remove_break (ebox_from_break_number break_number)
 
 let remove_break_from_ebox_number ebox_number =
-  let (click,bn,img,_) = List.assoc ebox_number !ebox_list in
-  remove_break click bn img
-
+  try
+    remove_break (Hashtbl.find event_boxes ebox_number)
+  with
+      Not_found -> raise No_such_event_box
+	
 let set_or_remove_break ebox_number callback1 callback2 =
-  let (click,bn,_,_) = List.assoc ebox_number !ebox_list in
-  if !click then
+  let {click = b; break_number = bn} =
+    (Hashtbl.find event_boxes ebox_number)
+  in
+  if b then
     (* new_breakpoint *)
     callback1 ()
   else
     (* remove_breakpoint *)
-    callback2 !bn ()
+    callback2 bn ()
 
